b9fcfcae463b8c1364ce53d54a464b654c9eed4e
Chadeus <49628319+0xChadeus@users.noreply.github.com>
2026-03-18 10:15:48 +1100

    Delete aws_push.sh

--- aws_push.sh
+++
@@ -1,3 +0,0 @@
-docker push 201063440280.dkr.ecr.ap-southeast-2.amazonaws.com/server:django_gunicorn
-docker push 201063440280.dkr.ecr.ap-southeast-2.amazonaws.com/server:nginx
-docker push 201063440280.dkr.ecr.ap-southeast-2.amazonaws.com/server:redis
